065d4d959f85106c7b9624d757b0780e1e76fcc3
Guilherme Condé <guicondee@outlook.com>
2023-01-08 17:49:06 -0300

    finished component challengeBox

--- src/components/ChallengeBox/ChallengeBox.tsx
+++ src/components/ChallengeBox/ChallengeBox.tsx
@@ -1,37 +1,50 @@
 import styles from '../ChallengeBox/ChallengeBox.module.css'
 
 export function ChallengeBox() {
+  const hasActiveChallenge = true;
   return (
     <div className={styles.challengeBoxContainer}>
-      <div className={styles.challengeNotActive}>
-        <strong>
-          Inicie um ciclo para receber desafios
-        </strong>
+      {hasActiveChallenge ? (
 
-        <p>
-          <img src="/icons/Icon.svg" alt="setinha pra cima verde" />
-          Avance de level completando desafios.
-        </p>
-      </div>
+        <div className={styles.challengeActive}>
+          <header>Ganhe 400 xp</header>
+          <main>
+            <img src="/icons/Group.svg" alt="icone mão segurando um peso" />
+            <strong>Novo desafio</strong>
+            <p>É agora Guilherme, bora lá meu parça.
+              Caminhe por 3 minutos e estique suas pernas
+              pra você ficar saudável.
+            </p>
+          </main>
+          <footer>
+            <button
+              type='button'
+              className={styles.challengeFailButton}
+            >
+              Falhei
+            </button>
+            <button
+              type='button'
+              className={styles.challengeSucceededButton}
+            >
+              Completei
+            </button>
+          </footer>
+        </div>
+      ) : (
+        <div className={styles.challengeNotActive}>
+          <strong>
+            Inicie um ciclo para receber desafios
+          </strong>
+
+          <p>
+            <img src="/icons/Icon.svg" alt="setinha pra cima verde" />
+            Avance de level completando desafios.
+          </p>
+        </div>
+      )}
     </div>
 
-    // <div className={s }>
-    //   <span>Ganhe 400 xp</span>
 
-    //   <div></div>
-
-    //   <div>
-    //     <img src="/icons/Group.svg" alt="icone mão segurando um peso" />
-    //     <strong>Exercite-se</strong>
-    //     <p>É agora Diegão, bora lá meu parça.
-    //       Caminhe por 3 minutos e estique suas pernas
-    //       pra você ficar saudável.</p>
-    //   </div>
-
-    //   <div>
-    //     <button>Falhei</button>
-    //     <button>Completei</button>
-    //   </div>
-    // </div>
   )
 }
