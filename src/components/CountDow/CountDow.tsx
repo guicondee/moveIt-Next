8b0374b9c9610bfe0a973c8f282e8813837cd412
Guilherme Condé <guicondee@outlook.com>
2023-01-14 12:19:46 -0300

    add context depencies

--- src/components/CountDow/CountDow.tsx
+++ src/components/CountDow/CountDow.tsx
@@ -1,46 +1,26 @@
 import { useContext, useEffect, useState } from 'react'
 import { ChallengesContext } from '../../contexts/ChallengerContext';
+import { CountdowContext } from '../../contexts/CountdowContext';
 
 import styles from '../CountDow/CountDow.module.css'
 
-let countdowTimeout: NodeJS.Timeout;
+
 
 export function CountDow() {
-  const { startNewChallenge } = useContext(ChallengesContext)
+  const { hasFinished,
+    isActive,
+    minutes,
+    seconds,
+    resetCountdow,
+    startCountdown
+  } = useContext(CountdowContext)
 
-  const [time, setTime] = useState(0.1 * 60);
-  const [isActive, setIsActive] = useState(false);
-  const [hasFinished, setHasFinished] = useState(false);
-
-  const minutes = Math.floor(time / 60)
-  const seconds = time % 60
 
   const [minuteLeft, minuteRight] = String(minutes).padStart(2, '0').split('')
   const [secondLeft, secondRight] = String(seconds).padStart(2, '0').split('')
 
-  function startCountdown() {
-    setIsActive(true)
-  }
 
-  function resetCountdow() {
-    clearTimeout(countdowTimeout)
-    setIsActive(false)
-    setTime(0.1 * 60)
 
-  }
-
-  useEffect(() => {
-    if (isActive && time > 0) {
-      countdowTimeout = setTimeout(() => {
-        setTime(time - 1)
-      }, 1000)
-
-    } else if (isActive && time === 0) {
-      setHasFinished(true);
-      setIsActive(false)
-      startNewChallenge()
-    }
-  }, [isActive, time])
 
 
   return (
@@ -83,17 +63,10 @@ export function CountDow() {
               onClick={startCountdown}
             >
               Iniciar um ciclo
-
             </button>
           }
         </>
       )}
-
-
-
-
-
-
     </div>
 
   )
