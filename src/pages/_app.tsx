3b707dcd51036cd833eeb5e9c4246a7ce56eb17e
Guilherme Condé <guicondee@outlook.com>
2023-01-14 12:22:34 -0300

    add provider context

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import '../styles/global.css'
 import type { AppProps } from 'next/app'
 
 import { ChallengesProvider } from '../contexts/ChallengerContext'
+import { CountdowContext } from '../contexts/CountdowContext'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   return (
+
     <ChallengesProvider>
       <Component {...pageProps} />
     </ChallengesProvider>
+
+
   )
 }
