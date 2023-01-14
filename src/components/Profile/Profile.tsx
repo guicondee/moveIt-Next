41e72e29aee10a6440f6904b8587026fd84636e7
Guilherme Condé <guicondee@outlook.com>
2023-01-14 12:21:03 -0300

    create context depencies

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -1,16 +1,20 @@
+import { useContext } from 'react'
+import { ChallengesContext } from '../../contexts/ChallengerContext'
 import styles from '../Profile/Profile.module.css'
 
 export function Profile() {
- return (
-  <div className={styles.profileContainer}>
-    <img src="https://github.com/guicondee.png" alt="Foto de perfil" />
-    <div >
-      <strong>Guilherme Condé</strong>
-      <p>
-        <img src="icons/level.svg" alt="Level" />
-        Level 1
-      </p>
+  const { level } = useContext(ChallengesContext)
+
+  return (
+    <div className={styles.profileContainer}>
+      <img src="https://github.com/guicondee.png" alt="Foto de perfil" />
+      <div >
+        <strong>Guilherme Condé</strong>
+        <p>
+          <img src="icons/level.svg" alt="Level" />
+          Level  {level}
+        </p>
+      </div>
     </div>
-  </div>
- )
+  )
 }
