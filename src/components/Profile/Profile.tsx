a1f31f8fe4f8af14b934e56b5ea5ae59f89c15f5
Guilherme Condé <102974715+guicondee@users.noreply.github.com>
2023-07-24 21:23:24 -0300

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -7,7 +7,7 @@ export function Profile() {
 
   return (
     <div className={styles.profileContainer}>
-      <img src="https://plus.unsplash.com/premium_photo-1683865775631-3283bfaf6508?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=80" alt="Foto de perfil" />
+      <img src="https://unsplash.com/pt-br/fotografias/mr26tQgHGmc" />
       <div >
         <strong>Guilherme Condé</strong>
         <p>
