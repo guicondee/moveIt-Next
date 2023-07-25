2ddd6cd5393578cc54b58e549653ca625bffbbac
Guilherme Condé <102974715+guicondee@users.noreply.github.com>
2023-07-24 21:26:41 -0300

    Update Profile.tsx

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -9,7 +9,7 @@ export function Profile() {
     <div className={styles.profileContainer}>
       <img src="https://images.unsplash.com/photo-1634034379073-f689b460a3fc?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=880&q=80" />
       <div >
-        <strong>Guilherme Condé</strong>
+        <strong>User 00038</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Level  {level}
